5f2c802c7a142dae00b3e4963886b4edaabd1d2f
Matthew R Becker <becker.mr@gmail.com>
2019-09-04 09:04:36 -0500

    try a bigger shear

--- work/piff_wl_sims/v002_exp_piff_jacwcs/config.py
+++ work/piff_wl_sims/v002_exp_piff_jacwcs/config.py
@@ -3,8 +3,8 @@ import numpy as np
 
 jc = galsim.ShearWCS(
     0.263,
-    galsim.Shear(g1=np.random.uniform(low=-0.01, high=0.01),
-                 g2=np.random.uniform(low=-0.01, high=0.01))).jacobian()
+    galsim.Shear(g1=np.random.uniform(low=-0.1, high=0.1),
+                 g2=np.random.uniform(low=-0.1, high=0.1))).jacobian()
 
 jacobian_dict = {
     'dudx': jc.dudx,
