8b7fbf236ade3a0a8ae31b612569229f601b35a4
Matthew R Becker <becker.mr@gmail.com>
2019-09-03 20:31:27 -0500

    try again

--- work/piff_wl_sims/v001_exp_piff/job.sh
+++ work/piff_wl_sims/v001_exp_piff/job.sh
@@ -17,7 +17,7 @@ fi
 for seed in `seq $1 $2`
 do
     python run.py ${seed} $tmpdir/out_${1}_${2}_${seed}.pkl &> $tmpdir/log_${1}_${2}_${seed}.oe
-    mv $tmpdir/log_${1}_${2}_${seed}.oe mv ${3}/log_${1}_${2}_${seed}.oe
+    mv $tmpdir/log_${1}_${2}_${seed}.oe ${3}/log_${1}_${2}_${seed}.oe
 done
 
 python collate.py ${3}/out_${1}_${2}.pkl $tmpdir/out_${1}_${2}_*
