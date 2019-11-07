9bf312ee16e8762f4c0a3218c11417a9fac9c50b
Matthew R Becker <becker.mr@gmail.com>
2019-11-07 14:45:20 -0600

    try this

--- work/parsl_condor/pctest.py
+++ work/parsl_condor/pctest.py
@@ -1,32 +1,52 @@
-import os
+import parsl
 from parsl.providers import CondorProvider
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
-from parsl.addresses import address_by_hostname
-proxy = '/tmp/x509up_u{}'.format(os.getuid())
-if not os.path.isfile(proxy):
-   raise RuntimeError('No valid proxy found-- please run voms-proxy-init -voms cms')
-worker_init = '''
-source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
-export PATH=~/.local/bin:$PATH
-export PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
-export X509_USER_PROXY=pwd/{}
-'''.format(os.path.basename(proxy))
-config = Config(
-   executors=[
-       HighThroughputExecutor(
-           address=address_by_hostname(),
-           cores_per_worker=1,
-           worker_debug=True,
-           provider=CondorProvider(
-               cores_per_slot=8,
-               init_blocks=80,
-               max_blocks=80,
-               worker_init=worker_init,
-               transfer_input_files=[proxy],
-           ),
-       )
-   ],
-   retries=5,
-   strategy=None
+
+from parsl.configs.local_threads import config
+
+SCHED_OPTS = ""
+WORKER_INIT = ""
+WALLTIME = ""
+
+condor_config = Config(
+    executors=[
+        HighThroughputExecutor(
+            worker_debug=True,
+            provider=CondorProvider(
+                cores_per_slot=1,
+                mem_per_slot=2,
+                nodes_per_block=1,
+                init_blocks=10,
+                max_blocks=10,
+                scheduler_options=SCHED_OPTS,
+                worker_init=WORKER_INIT,
+                walltime=WALLTIME
+            )
+        )
+    ],
 )
+
+parsl.load(config)
+
+
+@parsl.python_app
+def app_double(x):
+    return x*2
+
+
+@parsl.python_app
+def app_sum(inputs=[]):
+    return sum(inputs)
+
+
+items = range(0, 1000)
+
+mapped_results = []
+for i in items:
+    x = app_double(i)
+    mapped_results.append(x)
+
+total = app_sum(inputs=mapped_results)
+
+print(total.result())
