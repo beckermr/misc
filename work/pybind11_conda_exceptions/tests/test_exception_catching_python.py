b06b4b7228d0d09d9a4dbf127881f7184f7041f7
Matthew R. Becker <becker.mr@gmail.com>
2020-04-17 23:41:24 -0500

    better formatting

--- work/pybind11_conda_exceptions/tests/test_exception_catching_python.py
+++ work/pybind11_conda_exceptions/tests/test_exception_catching_python.py
@@ -6,5 +6,7 @@ import testLib
 try:
     testLib.fail("message1")
 except Exception as e:
-    print("\nERROR:", repr(e), "\n")
+    print("\nexception:", repr(e), "\n")
     assert repr(e) == "LSSTException('message1')"
+
+print("\nPASSED")
