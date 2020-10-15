84a401981750d6edd9beb8df669654a08dbd5f92
Matthew R. Becker <becker.mr@gmail.com>
2020-10-15 08:38:48 -0500

    make the stamps a bit different

--- work/blend_response/run_code.sh
+++ work/blend_response/run_code.sh
@@ -4,6 +4,10 @@ ns=10000
 mw=32
 python blend_code.py --max-workers=${mw} --shear=0.02 --n-samples=${ns}
 python blend_code.py --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
-
 python blend_code.py --blend --max-workers=${mw} --shear=0.02 --n-samples=${ns}
 python blend_code.py --blend --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
+
+python blend_code.py --max-workers=${mw} --shear=0.02 --n-samples=${ns} --top
+python blend_code.py --max-workers=${mw} --shear=-0.02 --n-samples=${ns} --top
+python blend_code.py --blend --max-workers=${mw} --shear=0.02 --n-samples=${ns} --top
+python blend_code.py --blend --max-workers=${mw} --shear=-0.02 --n-samples=${ns} --top
