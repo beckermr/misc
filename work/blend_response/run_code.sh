b8d2e5961fcb1acf84ab60f0b0bbef75294c54e6
Matthew R. Becker <becker.mr@gmail.com>
2020-10-15 10:12:03 -0500

    random orientation

--- work/blend_response/run_code.sh
+++ work/blend_response/run_code.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ns=10000
-mw=32
-python blend_code.py --max-workers=${mw} --shear=0.02 --n-samples=${ns}
-python blend_code.py --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
-python blend_code.py --blend --max-workers=${mw} --shear=0.02 --n-samples=${ns}
-python blend_code.py --blend --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
+mw=64
+# python blend_code.py --max-workers=${mw} --shear=0.02 --n-samples=${ns}
+# python blend_code.py --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
+# python blend_code.py --blend --max-workers=${mw} --shear=0.02 --n-samples=${ns}
+# python blend_code.py --blend --max-workers=${mw} --shear=-0.02 --n-samples=${ns}
 
 python blend_code.py --max-workers=${mw} --shear=0.02 --n-samples=${ns} --top
 python blend_code.py --max-workers=${mw} --shear=-0.02 --n-samples=${ns} --top
