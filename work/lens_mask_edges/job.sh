e431b2efb080083391e03baf3de1b477ff366642
Matthew R. Becker <becker.mr@gmail.com>
2021-06-20 15:24:45 -0500

    ENH add job sceipt

---
+++ work/lens_mask_edges/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=star-mask-truth
+#SBATCH --account=metashear
+#SBATCH --partition=bdwall
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --output=log_star-mask-truth-%j.oe
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+conda activate bebop
+
+python run.sh
--- work/lens_mask_edges/run.py
+++ work/lens_mask_edges/run.py
@@ -1,10 +1,12 @@
 #!/usr/bin.env python
 from mdet_tools import meas_m
+import sys
+
 
 if __name__ == "__main__":
     n_stars = 20
     m, merr, c, cerr, _, _ = meas_m(
-        n_stars=n_stars, mask_width=4, seed=20, n_jobs=5_000_000
+        n_stars=n_stars, mask_width=4, seed=20, n_jobs=int(sys.argv[1]),
     )
 
     print("\nstar density: %s [num per arcmin^2]" % (n_stars / (235*0.20/60)**2))
