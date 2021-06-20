d8b9054a4cc0620975b8c274bb0798aef9e4eec8
Matthew R. Becker <becker.mr@gmail.com>
2021-06-20 15:21:48 -0500

    change again

--- work/lens_mask_edges/run.py
+++ work/lens_mask_edges/run.py
@@ -2,4 +2,14 @@
 from mdet_tools import meas_m
 
 if __name__ == "__main__":
-    m, merr, c, cerr = meas_m(n_stars=20, mask_width=4, seed=20, n_jobs=100000)
+    n_stars = 20
+    m, merr, c, cerr, _, _ = meas_m(
+        n_stars=n_stars, mask_width=4, seed=20, n_jobs=5_000_000
+    )
+
+    print("\nstar density: %s [num per arcmin^2]" % (n_stars / (235*0.20/60)**2))
+    mstr = "m +/- merr: %0.6f +/- %0.6f [10^(-3), 3sigma]" % (m/1e-3, 3*merr/1e-3)
+    print(mstr, flush=True)
+
+    cstr = "c +/- cerr: %0.6f +/- %0.6f [10^(-5), 3sigma]" % (c/1e-3, 3*cerr/1e-3)
+    print(cstr, flush=True)
