a5e6d9d1118957b4923b7a5878021c568ff47edd
Matthew R. Becker <becker.mr@gmail.com>
2021-06-21 06:23:32 -0500

    ENH run with smaller mask edges

--- work/lens_mask_edges/run.py
+++ work/lens_mask_edges/run.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     n_stars = 20
     m, merr, c, cerr, _, _ = meas_m(
-        n_stars=n_stars, mask_width=4, seed=20, n_jobs=int(sys.argv[1]),
+        n_stars=n_stars, mask_width=2, seed=20, n_jobs=int(sys.argv[1]),
     )
 
     print("\nstar density: %s [num per arcmin^2]" % (n_stars / (235*0.20/60)**2))
