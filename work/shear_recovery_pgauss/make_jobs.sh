6cdbe1945f960240090ab2bb0663e202a94f6abe
Matthew R. Becker <becker.mr@gmail.com>
2021-11-11 15:35:56 -0600

    code to make jobs

--- work/shear_recovery_pgauss/make_jobs.sh
+++ work/shear_recovery_pgauss/make_jobs.sh
@@ -3,20 +3,20 @@
 for seed in 130 2314 34123 23431 23432 234 234321 8575 545243 563563; do
   echo $seed
 
-  echo "
-  #!/bin/bash
-  #SBATCH --job-name=pg${seed}
-  #SBATCH --account=metashear
-  #SBATCH --partition=bdwall
-  #SBATCH --nodes=1
-  #SBATCH --ntasks-per-node=1
-  #SBATCH --exclusive
-  #SBATCH --output=log_pg${seed}_lcrc%j.oe
-  #SBATCH --time=12:00:00
+  echo "\
+#!/bin/bash
+#SBATCH --job-name=pg${seed}
+#SBATCH --account=metashear
+#SBATCH --partition=bdwall
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --output=log_pg${seed}_lcrc%j.oe
+#SBATCH --time=12:00:00
 
-  source ~/.bashrc
-  conda activate bebop
+source ~/.bashrc
+conda activate bebop
 
-  srun python test_mdet.py --seed=${seed}
+srun python test_mdet.py --seed=${seed}
 " > job_${seed}.sh
 done
