e2b6ca56d01002a6825eb5202eaf56656a7506ef
Matthew R Becker <becker.mr@gmail.com>
2021-12-05 13:40:55 -0500

    parsl test script

--- work/parsl_condor/pctest.py
+++ work/parsl_condor/pctest.py
@@ -1,10 +1,16 @@
+import time
+import sys
 import parsl
 from parsl.providers import CondorProvider
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
-from parsl.addresses import address_by_hostname
+from parsl.addresses import address_by_hostname, get_all_addresses
+
+from concurrent.futures import as_completed
 
 from parsl.configs.local_threads import config
+
+START = time.time()
 
 SCHED_OPTS = """
 kill_sig        = SIGINT
@@ -41,14 +47,13 @@ WALLTIME = "48:00:00"
 condor_config = Config(
     executors=[
         HighThroughputExecutor(
-            worker_debug=False,
+            worker_debug=True,
             max_workers=1,
-            address=address_by_hostname(),
             provider=CondorProvider(
                 cores_per_slot=1,
                 mem_per_slot=2,
                 nodes_per_block=1,
-                init_blocks=1,
+                init_blocks=0,
                 parallelism=1,
                 max_blocks=10000,
                 scheduler_options=SCHED_OPTS,
@@ -66,22 +71,48 @@ parsl.load(condor_config)
 @parsl.python_app
 def app_double(x):
     import time
-    time.sleep(100)    
-    return x*2
+    time.sleep(30)    
+    return 2*x
 
 
-@parsl.python_app
-def app_sum(inputs=[]):
-    return sum(inputs)
-
-
-items = range(0, 5000)
+items = range(0, int(sys.argv[1]))
 
 mapped_results = []
 for i in items:
     x = app_double(i)
     mapped_results.append(x)
 
-total = app_sum(inputs=mapped_results)
 
-print(total.result())
+res = []
+n_tot = len(mapped_results)
+n_left = len(mapped_results)
+start = time.time()
+eta = -1
+while n_left > 0:
+    for i in range(len(mapped_results)):
+        f = mapped_results[i]
+        if n_left == 1:
+            print("done?", f.done(), flush=True)
+        try:
+            f.result(timeout=10)
+        except:
+            pass
+        if n_left == 1:
+            print("done?", f.done(), flush=True)
+        if f.done():
+            res.append(f.result())
+            n_left -= 1
+            eta = (time.time() - start) / (n_tot - n_left) * n_left
+            print("[%06ds] eta|done|left:" % (int(time.time() - START)), eta, n_tot - n_left, n_left, flush=True)
+            mapped_results.pop(i)
+            break
+            
+# for f in as_completed(mapped_results):
+#    n_left -= 1
+
+print("total:", sum(res), sum(2*x for x in range(0, int(sys.argv[1]))), flush=True)
+
+print("done w/ loop:", time.time() - start, flush=True)
+
+# total = app_sum(inputs=mapped_results)
+# print("total:", total.result(), flush=True)
