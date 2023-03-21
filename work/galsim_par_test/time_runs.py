546a66914b6233d61137995469048ded58e5354d
beckermr <becker.mr@gmail.com>
2023-03-21 16:06:22 -0500

    again

--- work/galsim_par_test/time_runs.py
+++ work/galsim_par_test/time_runs.py
@@ -4,13 +4,13 @@ import glob
 
 
 try:
-    subprocess.run(
-        "rm -f multi_image_mod_*.fits",
-        shell=True,
-        capture_output=True,
-    )
-
     for nproc in [2, 4, 8]:
+        subprocess.run(
+            "rm -f multi_image_mod_*.fits",
+            shell=True,
+            capture_output=True,
+        )
+
         t0 = time.time()
         subprocess.run(
             "galsim config.yml output.nproc=%d" % nproc,
@@ -20,8 +20,8 @@ try:
         t0 = time.time() - t0
         print(nproc, t0)
 
-    fnames = len(glob.glob("multi_image_mod_*.fits"))
-    assert fnames == 16, fnames
+        fnames = glob.glob("multi_image_mod_*.fits")
+        assert fnames == 16, fnames
 finally:
     subprocess.run(
         "rm -f multi_image_mod_*.fits",
