149fd44a9026f368dced08c198d698c351c9a99c
beckermr <becker.mr@gmail.com>
2023-03-21 16:03:55 -0500

    again

--- work/galsim_par_test/time_runs.py
+++ work/galsim_par_test/time_runs.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import glob
 
 
 try:
@@ -18,10 +19,11 @@ try:
         )
         t0 = time.time() - t0
         print(nproc, t0)
+
+    assert len(glob.glob("multi_image_mod_*.fits")) == 16
 finally:
-    pass
-    # subprocess.run(
-    #     "rm -f multi_image_mod_*.fits",
-    #     shell=True,
-    #     capture_output=True,
-    # )
+    subprocess.run(
+        "rm -f multi_image_mod_*.fits",
+        shell=True,
+        capture_output=True,
+    )
